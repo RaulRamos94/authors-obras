7052beca2f1ba6ac7a2a820ef782ff1312d41035
RaulRamos94 <raulfernandes3@hotmail.com>
2023-09-14 21:02:46 -0300

    Alteracao na index

--- BackEnd/src/main/java/com/example/AuthorsObras/repository/ObraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.AuthorsObras.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.AuthorsObras.model.Obra;
-
-public interface ObraRepository extends JpaRepository<Obra, Long>{
-    
-}
