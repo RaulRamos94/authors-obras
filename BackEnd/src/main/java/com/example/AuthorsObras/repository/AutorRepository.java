5b43be7ddc48975e05d6ca4e7e50ea330b9ccfd4
RaulRamos94 <raulfernandes3@hotmail.com>
2023-09-14 21:20:36 -0300

    Remocao de bibliotecas nao utilizadas

--- BackEnd/src/main/java/com/example/AuthorsObras/repository/AutorRepository.java
+++ BackEnd/src/main/java/com/example/AuthorsObras/repository/AutorRepository.java
@@ -1,9 +1,6 @@
 package com.example.AuthorsObras.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.AuthorsObras.model.Autor;
 
